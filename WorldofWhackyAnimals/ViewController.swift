be218f0cc5f0fee6596039941830522ecd523464
Anthony Rella <anthonyrella@outlook.com>
2018-11-17 18:52:17 -0500

    Empty project

---
+++ WorldofWhackyAnimals/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WorldofWhackyAnimals
+//
+//  Created by Anthony Rella on 2018-11-17.
+//  Copyright © 2018 AnthonyRella. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
